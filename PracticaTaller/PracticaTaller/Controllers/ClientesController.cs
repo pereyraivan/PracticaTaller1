35cf089e4a84ecc8ae9f27ce3af506624b2b38a9
pereyraivan <pereyraivan@hotmail.com>
2017-02-11 22:08:59 -0300

    Ventas

---
+++ PracticaTaller/PracticaTaller/Controllers/ClientesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PracticaTaller.Models.LogicaNegocio;
+using PracticaTaller.ViewModels;
+using PracticaTaller.Models.AccesoDatos;
+
+namespace PracticaTaller.Controllers
+{
+    public class ClientesController : Controller
+    {
+        GestorCliente gestorCliente = new GestorCliente();
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            var listaClientes = gestorCliente.Listar();
+            return View(listaClientes);
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(ClienteViewModel cliente)
+        {
+            var nuevoCliente = new Cliente
+            {
+                Apellido = cliente.Apellido,
+                Nombre = cliente.Nombre,
+                nro_dni = cliente.nro_dni
+            };
+
+            gestorCliente.Guardar(nuevoCliente);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var clienteModificar = gestorCliente.ObtenerPorId(id);
+            return View(clienteModificar);
+        }
+
+        public ActionResult GuardarModificacion(Cliente cliente)
+        {
+            gestorCliente.Modificar(cliente);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            gestorCliente.Eliminar(id);
+            return RedirectToAction("Listar");
+        }
+
+    }
+}
---
+++ PracticaTaller/PracticaTaller/Scripts/ScriptVenta.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("tr.registro-venta").click(function () {
+        var id = $(this).find("td.id-venta").html();
+        $("tr.registro-detalle").hide();
+        $("tr.registro-detalle").has("td.id-venta:contains('" + id + "')").show();
+    });
+    $("#txt-buscar").on('input', function () {
+
+        var nombreBuscado = $("#txt-buscar").val().toUpperCase();
+
+        $(".producto").each(function (index, data) {
+
+            var nombreProducto = ($(this).find("td.nombre").html().toUpperCase());
+
+            if (nombreProducto.startsWith(nombreBuscado)) {
+                $(this).show();
+
+            }
+            else {
+
+                $(this).hide();
+            }
+        });
+        var cantidadVisibles = $(".producto:visible").length;
+        console.log(cantidadVisibles);
+        if (cantidadVisibles == 1) {
+            $('#boton').attr("disabled", false);
+        }
+        else {
+            $('#boton').attr("disabled", true);
+        }
+
+    });
+});
